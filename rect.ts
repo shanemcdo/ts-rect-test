99714f23967ec6badd9ccd306b2f4012c01581c4
Shane McDonough <shanemcdman@gmail.com>
2023-12-12 10:08:06 -0800

    Add more setters and getters

--- rect.ts
+++ rect.ts
@@ -20,12 +20,59 @@ class Rect {
 		this.height = height;
 	}
 
-	get topLeft(): [number, number] {
-		return [this.left, this.height];
+	get right(): number {
+		return this.left + this.width;
+	}
+	
+	set right(right: number) {
+		this.left = right - this.width;
 	}
 
-	set topLeft([left, height]: [number, number]) {
-		this.left = left;
-		this.height = height;
+	get bottom(): number {
+		return this.top + this.height;
+	}
+
+	set bottom(bottom: number) {
+		this.top = bottom - this.height;
+	}
+
+	get centerX(): number {
+		return this.left + this.width / 2;
+	}
+
+	set centerX(center: number) {
+		this.left = center - this.width / 2;
+	}
+
+	get centerY(): number {
+		return this.top + this.width / 2
+	}
+
+	set centerY(center: number) {
+		this.top = center - this.width / 2;
+	}
+
+	get topLeft(): [number, number] {
+		return [this.left, this.top];
+	}
+
+	set topLeft(topLeft: [number, number]) {
+		[this.left, this.top] = topLeft;
+	}
+
+	get bottomRight(): [number, number] {
+		return [this.right, this.bottom];
+	}
+
+	set bottomRight(bottomRight: [number, number]) {
+		[this.right, this.bottom] = bottomRight;
+	}
+
+	get center(): [number, number] {
+		return [this.centerX, this.centerY];
+	}
+
+	set center(center: [number, number]) {
+		[this.centerX, this.centerY] = center;
 	}
 }
