57db4381a899467013fa48fa8dd0fba51745c2d1
Shane McDonough <shanemcdman@gmail.com>
2023-12-12 09:56:44 -0800

    Initial

---
+++ rect.ts
@@ -0,0 +1,31 @@
+class Rect {
+	top: number;
+	left: number;
+	width: number;;
+	height: number;
+
+	constructor(top: number, left: number, width: number, height: number) {
+		this.top = top;
+		this.left = left;
+		this.width = width;
+		this.height = height;
+	}
+
+	get size(): [number, number] {
+		return [this.width, this.height];
+	}
+
+	set size([width, height]: [number, number]) {
+		this.width = width;
+		this.height = height;
+	}
+
+	get topLeft(): [number, number] {
+		return [this.left, this.height];
+	}
+
+	set topLeft([left, height]: [number, number]) {
+		this.left = left;
+		this.height = height;
+	}
+}
